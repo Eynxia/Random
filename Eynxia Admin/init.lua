791356dd91399ca8664c39ddc5e1d8a9fe43c362
Eynxia <cemallost2371@gmail.com>
2024-12-07 21:10:22 +0300

    Update init.lua

--- Eynxia Admin/init.lua
+++ Eynxia Admin/init.lua
@@ -4,23 +4,36 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 
 --|| Other
 
+local Client = script:WaitForChild("Client")
+local Client_Server = script:WaitForChild("Client|Server")
+local Fetch = Client_Server:WaitForChild("fetch")
+local Chat = Client:WaitForChild("Chat")
 local Main = Instance.new("Folder")
 local Folder = Instance.new("Folder")
-local Event = Instance.new("RemoteEvent")
+local Event = Instance.new("RemoteEvent") 
 local fetch = Instance.new("Folder")
 local __Cmds = Instance.new("Folder")
+local Server = script:WaitForChild("Server")
+local __HANDLER = Server:WaitForChild("_Handler")
 
 Main.Name = "Main"
 fetch.Name = "return"
 Folder.Name = "Event"
 Event.Name = "Fire"
 __Cmds.Name = "_cmds"
+fetch.Name = "Main"
 
 __Cmds.Parent = ServerScriptService
+__HANDLER.Parent = __Cmds
 Main.Parent = ReplicatedStorage
-fetch.Name = Main
 Folder.Parent = Main
 Event.Parent = Folder
+
+Fetch.Parent = fetch
+Chat.Parent = ReplicatedFirst
+Chat.Enabled = true
+__HANDLER.Enabled = true
