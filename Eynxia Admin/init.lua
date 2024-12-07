a5b173f7529200e2c59235921bda9959f831923f
Eynxia <cemallost2371@gmail.com>
2024-12-07 20:16:03 +0300

    Update init.lua

--- Eynxia Admin/init.lua
+++ Eynxia Admin/init.lua
@@ -3,6 +3,7 @@
 --|| Services
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
 
 --|| Other
 
@@ -10,12 +11,15 @@ local Main = Instance.new("Folder")
 local Folder = Instance.new("Folder")
 local Event = Instance.new("RemoteEvent")
 local fetch = Instance.new("Folder")
+local __Cmds = Instance.new("Folder")
 
 Main.Name = "Main"
 fetch.Name = "return"
 Folder.Name = "Event"
 Event.Name = "Fire"
+__Cmds.Name = "_cmds"
 
+__Cmds.Parent = ServerScriptService
 Main.Parent = ReplicatedStorage
 fetch.Name = Main
 Folder.Parent = Main
