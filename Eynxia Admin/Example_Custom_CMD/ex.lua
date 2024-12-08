87593a3a482018e6de97d939fe00b71eea3034e8
Eynxia <cemallost2371@gmail.com>
2024-12-08 22:13:48 +0300

    Update ex.lua

--- Eynxia Admin/Example_Custom_CMD/ex.lua
+++ Eynxia Admin/Example_Custom_CMD/ex.lua
@@ -10,13 +10,5 @@ target_list_or_variables = array
 
 local TYPE_ = typeof(array)
 
-if TYPE_ == "table" then
-	for p,d in TYPE_ do
-		warn("\n\nplayer that got into the target list: "..d.Name)
-	end
-	
-elseif TYPE_ == "Instance" then
-	warn("\n\nSince the target list was empty changed it to the default value which is the person who fire the remote event.")
-end
 
-warn("\n\nVariables:\n\n| Player List |: ",Player_list,"\nPerson that fired the remote event: "..person_Who_fired_The_event.Name.."\ntarget & variable list: ",target_list_or_variables)
+warn("\n\nVariables:\n\n| Player List |: ",Player_list or nil,"\nPerson that fired the remote event: "..person_Who_fired_The_event.Name.."\ntarget & variable list: ",target_list_or_variables or nil)
