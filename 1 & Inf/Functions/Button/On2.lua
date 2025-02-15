be3a5d8a50c6c62553358d47b94ff197e4a74875
Eynxia <cemallost2371@gmail.com>
2025-02-15 18:44:42 +0300

    Create On2.lua

---
+++ 1 & Inf/Functions/Button/On2.lua
@@ -0,0 +1,28 @@
+local Variables = shared["Variables"]
+local IsOn = Variables["ForceUpgrade"]
+
+local Mouse:Mouse = __Client:GetMouse()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ForceUpgrade = ReplicatedStorage:WaitForChild("ForceUpgrade")
+
+if not IsOn then
+	Variables["ForceUpgrade"] = true
+else
+	Variables["ForceUpgrade"] = false
+end
+
+if Variables["MouseConnection"] then return end
+
+shared["Variables"]["MouseConnection"] = Mouse.Button1Down:Connect(function()
+	if not IsOn then return end
+
+	local Target = Mouse.Target
+	if not Target then return end
+
+	local Name = Target["Name"]
+
+	if tonumber(Name) then
+		ForceUpgrade:FireServer(Target)
+	end
+end)
