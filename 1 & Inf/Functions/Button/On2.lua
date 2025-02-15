f3d2b47c19f50fca34e3cab147e49cbae49a0485
Eynxia <cemallost2371@gmail.com>
2025-02-15 20:08:11 +0300

    Update On2.lua

--- 1 & Inf/Functions/Button/On2.lua
+++ 1 & Inf/Functions/Button/On2.lua
@@ -17,8 +17,8 @@ if Variables["MouseConnection"] then return end
 
 shared["Variables"]["MouseConnection"] = Mouse.Button1Down:Connect(function()
 		print("click clicky")
+		print(IsOn)
 	if not IsOn then return end
-
 	local Target = Mouse.Target
 	if not Target then return end
 
