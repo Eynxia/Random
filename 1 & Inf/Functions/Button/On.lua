4c84efaf9aa317f195dac63ad29d5642f80de276
Eynxia <cemallost2371@gmail.com>
2025-02-15 20:31:43 +0300

    Update On.lua

--- 1 & Inf/Functions/Button/On.lua
+++ 1 & Inf/Functions/Button/On.lua
@@ -1,15 +1,13 @@
 local Variables = shared["Variables"]
-local IsOn = Variables["Strong"]
+local IsOn = Debounce
 
 if not IsOn then
-	Variables["Strong"] = true
 	for Index,Value in __BodyParts do
 		if Value:IsA("BasePart") then
 			Value.CustomPhysicalProperties = PhysicalProperties.new(100,1,1)
 		end
 	end
 else
-	Variables["Strong"] = false
 	for Index,Value in __BodyParts do
 		if Value:IsA("BasePart") then
 			Value.CustomPhysicalProperties = nil
