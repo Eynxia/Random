1d864a8daca626c7a3601f88ea043311037549d2
Eynxia <cemallost2371@gmail.com>
2025-02-16 14:15:01 +0300

    Update On2.lua

--- 1 & Inf/Functions/Button/On2.lua
+++ 1 & Inf/Functions/Button/On2.lua
@@ -6,11 +6,9 @@ local Mouse:Mouse = game.Players.LocalPlayer:GetMouse()
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ForceUpgrade = ReplicatedStorage:WaitForChild("ForceCombine")
 
-if Variables["MouseConnection"] then return end
+if Variables["MouseConnection"] then Variables["MouseConnection"]:Disconnect() end
 
 shared["Variables"]["MouseConnection"] = Mouse.Button1Down:Connect(function()
-		print("click clicky")
-		print(IsOn)
 	if not IsOn then return end
 	local Target = Mouse.Target
 	if not Target then return end
